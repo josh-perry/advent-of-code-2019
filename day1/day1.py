e4645ecaa816e2f5418d6868d89272c44e3f5863
Josh Perry <joshperry@outlook.com>
2019-12-02 19:09:07 +0000

    Day 1, part 2 - calculate fuel consumption for fuel

--- day1/day1.py
+++ day1/day1.py
@@ -1,15 +1,28 @@
 import math
 
 
-def get_mass_fuel_requirement(mass):
+def get_fuel_for_mass(mass):
     return math.floor((mass / 3)) - 2
+
+
+def get_total_fuel_for_mass(mass):
+    total_fuel = 0
+
+    while True:
+        fuel = max(get_fuel_for_mass(mass), 0)
+        total_fuel += fuel
+
+        mass = fuel
+
+        if fuel <= 0:
+            return total_fuel
 
 
 def get_fuel_requirement(masses):
     total_fuel = 0
 
     for mass in masses:
-        total_fuel += get_mass_fuel_requirement(mass)
+        total_fuel += get_total_fuel_for_mass(mass)
 
     return total_fuel
 
--- tests/test_day1.py
+++ tests/test_day1.py
@@ -6,15 +6,14 @@ from day1 import day1
 class TestSum(unittest.TestCase):
     def test_fuel_calculation(self):
         param_list = [
-            (12, 2),
             (14, 2),
-            (1969, 654),
-            (100756, 33583)
+            (1969, 966),
+            (100756, 50346)
         ]
 
         for i, expected in param_list:
             with self.subTest():
-                fuel = day1.get_mass_fuel_requirement(i)
+                fuel = day1.get_total_fuel_for_mass(i)
                 self.assertEqual(fuel, expected)
 
 
